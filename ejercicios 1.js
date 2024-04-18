9a72dabdd6b505fae1eb71b3dddfce7ae60af3d1
pablo francone <166070187+PabloFrancone@users.noreply.github.com>
2024-04-17 23:35:03 -0300

    actvidad 3 (p1yp2) y 7

--- ejercicios 1.js
+++ ejercicios 1.js
@@ -12,4 +12,36 @@ let numb = 1
 let numb2 = 2
 let numb3 = 3
 
+mayor = 0
+menor = 99999999999999
 
+if(numb<numb2)  	{}
+
+act3
+
+let palabra = "jusancito"
+
+let n = 0
+
+n = palabra.length
+ 
+console.log (`la palabra ${palabra} posee ${n} lestras`)
+
+
+
+//ejercicio
+
+const pal="locura"
+console.log( pal, "Esta plabra tiene"+ pal.length+"letras")
+
+
+//mostar los multiplos de 2 y 5 menores a 100
+console.log(`los multiplos de 2 son`)
+for (let dosx=2; dosx<102; dosx=dosx + 2){
+    console.log (dosx)
+}
+
+console.log(`losmultiplos de 5 son`)
+for (let cincox=5; cincox<105; cincox=cincox + 5){
+    console.log(cincox)
+}
